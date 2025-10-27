e7851241ee50b57b714461f6d5bd4f89e408399a
RogerAlamanac <roger.alamanac@enti.cat>
2025-10-27 16:04:53 +0100

    recycler view fet

--- app/src/main/java/com/example/aa2_ahorcado_rogeralamanac/LevelSelector.kt
+++ app/src/main/java/com/example/aa2_ahorcado_rogeralamanac/LevelSelector.kt
@@ -1,11 +1,36 @@
 package com.example.aa2_ahorcado_rogeralamanac
 
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import models.Level
+import models.LevelAdapter
 
 class LevelSelector : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.level_selector)
+
+        val levels = listOf(
+            Level("ENTI", "4", R.drawable.ic_launcher_foreground),
+            Level("ENTI", "4", R.drawable.ic_launcher_foreground),
+            Level("ENTI", "4", R.drawable.ic_launcher_foreground),
+            Level("ENTI", "4", R.drawable.ic_launcher_foreground),
+            Level("ENTI", "4", R.drawable.ic_launcher_foreground),
+            Level("ENTI", "4", R.drawable.ic_launcher_foreground),
+            Level("ENTI", "4", R.drawable.ic_launcher_foreground),
+            Level("ENTI", "4", R.drawable.ic_launcher_foreground),
+            Level("ENTI", "4", R.drawable.ic_launcher_foreground),
+            Level("ENTI", "4", R.drawable.ic_launcher_foreground),
+            Level("ENTI", "4", R.drawable.ic_launcher_foreground),
+            Level("ENTI", "4", R.drawable.ic_launcher_foreground),
+            Level("ENTI", "4", R.drawable.ic_launcher_foreground)
+        )
+
+        val recyclerView: RecyclerView = findViewById(R.id.levels_recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = LevelAdapter(levels)
     }
 }
---
+++ app/src/main/java/models/LevelAdapter.kt
@@ -0,0 +1,38 @@
+package models
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.aa2_ahorcado_rogeralamanac.R
+
+class LevelAdapter(private val levels: List<Level>) : RecyclerView.Adapter<LevelAdapter.LevelViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LevelAdapter.LevelViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.recycler_layout_manager, parent, false)
+
+        return LevelViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: LevelAdapter.LevelViewHolder, position: Int) {
+        val level = levels[position]
+        holder.bind(level)
+    }
+
+    override fun getItemCount(): Int {
+        return levels.size
+    }
+
+    class LevelViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        private val levelWord: TextView = itemView.findViewById(R.id.textWord)
+        private val letterCount: TextView = itemView.findViewById(R.id.textLettersCount)
+        private val imageDifficulty: ImageView = itemView.findViewById(R.id.imageDifficulty)
+
+        fun bind(level:Level){
+            levelWord.text = level.word
+            letterCount.text = level.letterNum
+            imageDifficulty.setImageResource(level.imageDifficulty)
+        }
+    }
+}
